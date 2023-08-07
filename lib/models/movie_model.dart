08cdacb2655c490e44bbb2e348f3c057135eb9d2
lavfreits <llimafreitas@icloud.com>
2023-08-07 18:23:16 -0300

    featur: implementing model and requestApi

---
+++ lib/models/movie_model.dart
@@ -0,0 +1,32 @@
+class MovieModel {
+  final bool adult;
+  final String backdropPath;
+  final List<int> genreIds;
+  final int id;
+  final String originalLanguage;
+  final String originalTitle;
+  final String overview;
+  final double popularity;
+  final String posterPath;
+  final DateTime releaseDate;
+  final String title;
+  final bool video;
+  final double voteAverage;
+  final int voteCount;
+
+  MovieModel.fromMap(Map<String, dynamic> json):
+        adult = json["adult"],
+        backdropPath = json["backdrop_path"],
+        genreIds = List<int>.from(json["genre_ids"].map((x) => x)),
+        id = json["id"],
+        originalLanguage = json["original_language"],
+        originalTitle = json["original_title"],
+        overview = json["overview"],
+        popularity = json["popularity"]?.toDouble(),
+        posterPath = json["poster_path"],
+        releaseDate = DateTime.parse(json["release_date"]),
+        title = json["title"],
+        video = json["video"],
+        voteAverage = json["vote_average"]?.toDouble(),
+        voteCount = json["vote_count"];
+}
--- lib/pages/home_page/home_page.dart
+++ lib/pages/home_page/home_page.dart
@@ -1,15 +1,66 @@
 import 'package:flutter/material.dart';
+import 'lib/data/data_fetch.dart';
+import 'dart:ui';
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  bool isLoading = true;
+  List<MovieModel> movies = [];
+
+  @override
+  void initState() {
+    requestApi();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Color(0xFF540BA1),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment(0.00, -1.00),
+            end: Alignment(0, 1),
+            colors: [
+              Color(0xFF540BA1),
+              Colors.black.withOpacity(0.9),
+            ],
+            stops: [0, 0.6],
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.5),
+              spreadRadius: 5,
+              blurRadius: 7,
+              offset: Offset(0, 3),
+            ),
+          ],
+        ),
+        child: isLoading
+            ? Center(
+          child: CircularProgressIndicator(),
+        )
+            : ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: movies.length,
+          itemBuilder: (context, index) {
+            final movie = movies[index];
+            return MovieTile(movie: movie);
+          },
+        ),
+      ),
+    );
   }
 }
+Future<void> requestApi() async {
+    final movies = await fetchData(); // Assuming fetchData returns List<MovieModel>
+    setState(() {});
+  }
+
